ac913cad191f5602fece2d75bdfbd7e253030bd6
Jonathan <mcjhona069@gmail.com>
2020-10-06 16:08:16 -0500

    Creando las rutas de nuestra aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,12 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+
+//IMPORTAR RUTAS
+
+import { from } from 'rxjs';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -18,7 +25,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot( ROUTES, {useHash: true} )
   ],
   providers: [],
   bootstrap: [AppComponent]
