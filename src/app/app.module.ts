e8200360df0a543822691a6cba55a61606656f9a
Jonathan <mcjhona069@gmail.com>
2020-10-07 19:42:27 -0500

    Verificacion de img y pipe para manejar las imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { from } from 'rxjs';
 import { ROUTES } from './app.routes';
 
+//PIPES
+import { NoimagePipe } from './pipes/noimage.pipe';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +27,8 @@ import { ROUTES } from './app.routes';
     HomeComponent,
     SearchComponent,
     ArtistaComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe
   ],
   imports: [
     BrowserModule,
