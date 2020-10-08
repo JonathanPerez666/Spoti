eaa7c5a37fed9407c45a715adfae306ab136001d
Jonathan <mcjhona069@gmail.com>
2020-10-07 20:41:29 -0500

    Servicio Rop-Tracks

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -11,13 +11,15 @@ import { SpotifyService } from '../../services/spotify.service';
 export class ArtistaComponent {
 
   artista: any={};
+  topTracks: any[]=[];
   loadingArtist: boolean;
 
   constructor(private router: ActivatedRoute,
               private spotify: SpotifyService) {
         this.loadingArtist=true;
         this.router.params.subscribe(params => {
-        this.getArtista(params['id']); 
+          this.getArtista(params['id']);
+          this.getTopTracks(params['id']);
 
     });
    }
@@ -28,6 +30,12 @@ export class ArtistaComponent {
       .subscribe(artista=>{
         this.artista=artista;
         this.loadingArtist=false;
+      });
+  }
+  getTopTracks(id: string){
+    this.spotify.getTopTracks(id)
+      .subscribe(topTracks=>{
+        this.topTracks=topTracks;
       });
   }
 }
